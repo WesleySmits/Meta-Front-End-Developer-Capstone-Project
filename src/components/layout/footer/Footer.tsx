5be02249b3f5482084aa979a948f7c0528d1939a
Wesley Smits <wesleysmits@icloud.com>
2025-01-15 12:31:05 +0300

    Styling fixes

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -8,45 +8,47 @@ const Footer: React.FC = () => {
 
     return (
         <footer className={styles.footer}>
-            <div className={styles.footerColumns}>
-                <section>
-                    <img src="/logo-vertical.png" alt="Little Lemon Logo" width="200" height="352" />
-                </section>
+            <div className={`container ${styles.container}`}>
+                <div className={styles.footerColumns}>
+                    <section>
+                        <img src="/logo-vertical.png" alt="Little Lemon Logo" width="200" height="352" />
+                    </section>
 
-                <section>
-                    <h2>Navigation</h2>
-                    <nav>
-                        <ul>
-                            {NavigationMenu.map((item, index) => {
-                                return (
-                                    <li key={item.label + index}>
-                                        <NavLink to={item.href}>{item.label}</NavLink>
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </nav>
-                </section>
+                    <section>
+                        <h2>Navigation</h2>
+                        <nav>
+                            <ul>
+                                {NavigationMenu.map((item, index) => {
+                                    return (
+                                        <li key={item.label + index}>
+                                            <NavLink to={item.href}>{item.label}</NavLink>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </nav>
+                    </section>
 
-                <section>
-                    <h2>Contact</h2>
+                    <section>
+                        <h2>Contact</h2>
 
-                    <address>
-                        <p>1234 Lemon St.</p>
-                        <p>Lemon City, LC 12345</p>
-                        <p>
-                            <a href="tel:+1234567890">123-456-7890</a>
-                        </p>
-                        <p>
-                            <a href="mailto:someone@little-lemon-capstone-project.io">
-                                someone@little-lemon-capstone-project.io
-                            </a>
-                        </p>
-                    </address>
-                </section>
+                        <address>
+                            <p>1234 Lemon St.</p>
+                            <p>Lemon City, LC 12345</p>
+                            <p>
+                                <a href="tel:+1234567890">123-456-7890</a>
+                            </p>
+                            <p>
+                                <a href="mailto:someone@little-lemon-capstone-project.io">
+                                    someone@little-lemon-capstone-project.io
+                                </a>
+                            </p>
+                        </address>
+                    </section>
+                </div>
+
+                <small>&copy; {year} Little Lemon</small>
             </div>
-
-            <small>&copy; {year} Little Lemon</small>
         </footer>
     );
 };
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -13,26 +13,28 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ isMobileMenuOpen, setMobileMenuOpen }) => {
     return (
         <header className={styles.header}>
-            <Link to="/" className={styles.logo}>
-                <img src="/logo.png" alt="Little Lemon Logo" width="200" height="55" />
-            </Link>
+            <div className={`container ${styles.container}`}>
+                <Link to="/" className={styles.logo}>
+                    <img src="/logo.png" alt="Little Lemon Logo" width="200" height="55" />
+                </Link>
 
-            <DesktopNavigation className={styles.desktopNavigation} />
+                <DesktopNavigation className={styles.desktopNavigation} />
 
-            <div className={styles.ctaButtons}>
-                <ButtonLink href="/reserve" variant={ButtonVariant.Secondary} label="Reserve a Table" />
-                <LanguageSwitcher />
+                <div className={styles.ctaButtons}>
+                    <ButtonLink href="/reserve" variant={ButtonVariant.Secondary} label="Reserve a Table" />
+                    <LanguageSwitcher />
+                </div>
+
+                <button
+                    className={`${styles.mobileMenuButton} ${isMobileMenuOpen ? styles.open : ''}`}
+                    onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+                    data-testid="mobile-menu-button"
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
             </div>
-
-            <button
-                className={`${styles.mobileMenuButton} ${isMobileMenuOpen ? styles.open : ''}`}
-                onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
-                data-testid="mobile-menu-button"
-            >
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
         </header>
     );
 };
